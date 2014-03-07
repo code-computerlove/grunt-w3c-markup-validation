75b3ca0fd6e477065ad6d51f30cb5202dc9876e9
iainjmitchell <iainjmitchell@gmail.com>
2014-03-07 16:38:39 +0000

    validating one file by name

--- tasks/src/markup-validator.js
+++ tasks/src/markup-validator.js
@@ -1,7 +1,9 @@
 var w3cValidator = require('w3cjs');
 
 module.exports = function(){
-	this.validate = function(){
-		w3cValidator.validate();
+	this.validate = function(options){
+		w3cValidator.validate({
+			file : options.files[0]
+		});
 	};
 };
--- test/markupValidatorTests.js
+++ test/markupValidatorTests.js
@@ -14,5 +14,23 @@ test('When one file is validated Then w3c validation performed', function(done){
 		};
 	W3cMarkupValidationPlugin.__set__("w3cValidator", mockW3c);
 
-	new W3cMarkupValidationPlugin(fakeLog).validate(oneFile);
+	new W3cMarkupValidationPlugin(fakeLog).validate({
+		files: oneFile
+	});
+});
+
+test('When one file is validated Then w3c validation performed on file by name', function(done){
+	var fileName = 'random file ' + Math.random(),
+		oneFile = [fileName],
+		mockW3c = {
+			validate : function(options){
+				options.file.should.equal(fileName);
+				done();
+			}
+		};
+	W3cMarkupValidationPlugin.__set__("w3cValidator", mockW3c);
+
+	new W3cMarkupValidationPlugin(fakeLog).validate({
+		files: oneFile
+	});
 });
