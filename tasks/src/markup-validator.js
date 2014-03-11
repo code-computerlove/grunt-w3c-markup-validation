8a939fa3c2b74df61843bb17e75205c33fda21b6
iainjmitchell <iainjmitchell@gmail.com>
2014-03-11 10:01:09 +0000

    refactoring

--- tasks/src/markup-validator.js
+++ tasks/src/markup-validator.js
@@ -2,18 +2,32 @@ var w3cValidator = require('w3cjs');
 
 var MarkupValidator = function(log){
 	this.validate = function(options){
-		options.files.forEach(function(file){
-			w3cValidator.validate({
-				file : file,
-				callback : function(results){
-					if (!!results){
-						var error = results.messages[0],
-							errorMessage = file + ' | line ' + error.lastLine + ' | ' + error.message;
-						log.error(errorMessage);
-					}
-				}
-			});
+		options.files.forEach(function(pageUriOrFile){
+			new WebPageMarkupValidator(pageUriOrFile, log).validate();
 		});
+	};
+};
+
+var WebPageMarkupValidator = function(pageUriOrFile, log){
+	var w3cErrorDisplay = new W3cErrorDisplay(log, pageUriOrFile);
+
+	this.validate = function(){
+		w3cValidator.validate({
+			file : pageUriOrFile,
+			callback : function(results){
+				if (!!results){
+					var error = results.messages[0];
+					w3cErrorDisplay.show(error);
+				}
+			}
+		});
+	};
+};
+
+var W3cErrorDisplay = function(log, file){
+	this.show = function(error){
+		var errorMessage = file + ' | line ' + error.lastLine + ' | ' + error.message;
+		log.error(errorMessage);
 	};
 };
 
