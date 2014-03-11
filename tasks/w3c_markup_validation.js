8bd3149b8aa62312b940c1ec6bf6980494e29c73
iainjmitchell <iainjmitchell@gmail.com>
2014-03-11 10:18:39 +0000

    refactoring

--- tasks/w3c_markup_validation.js
+++ tasks/w3c_markup_validation.js
@@ -1,13 +1,13 @@
 var MarkupValidator = require('src/markup-validator');
 
 module.exports = function(grunt) {
-  var markupValidator = new MarkupValidator(grunt.log);
+    var markupValidator = new MarkupValidator(grunt.log);
 
-  grunt.registerMultiTask('w3c_markup_validation', 'Grunt task for W3C validation of markup', function() {
-    var done = this.async();
-    markupValidator.validate({
-      files : this.files, 
-      validateOptions : this.options
-    }, done);
+    grunt.registerMultiTask('w3c_markup_validation', 'Grunt task for W3C validation of markup', function() {
+        var done = this.async();
+        markupValidator.validate({
+            files : this.files, 
+            validateOptions : this.options
+        }, done);
   });
 };
